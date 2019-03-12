a389579a77d08fed7fd02d946797dd199f77a9ef
Oleg Stobbe <oleg@openslx.com>
2019-03-12 16:39:46 +0100

    add helper script for initialization of ansible-controller

---
+++ scripts/prepare.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env sh
+
+set -e
+
+__newline() {
+    echo ''
+}
+
+__info() {
+    echo "[INFO] $1"
+}
+
+
+cd "$(cd "$(dirname "$0")/.." && pwd -P)"
+repodir="${PWD}"
+
+__info "preparing repo-directory: ${repodir}"
+
+__info 'preparing subdirectories...'
+mkdir -v -p artifacts artifacts/config
+
+__info 'checking out git-submodules...'
+git submodule update --init --recursive --depth 1
+
+# check if sudo is required to run docker
+docker info > /dev/null 2>&1 || sudocmd='sudo'
+
+# build required docker-containers...
+for image in alpine ansible pwdgen ssh-keygen ; do
+    cd "${repodir}/eaas/ansible/docker/${image}"
+    __info "building ${image} docker-container..."
+    ${sudocmd} ./build-image.sh
+    __newline
+done
+
+cd "${repodir}"
+
+keydir='./artifacts/ssh'
+key="${keydir}/admin.key"
+if [ ! -e "${key}" ] ; then
+    __info 'generating ssh-key...'
+    mkdir -v -p "${keydir}"
+    ${sudocmd} ./eaas/ansible/scripts/ssh-keygen.sh "${key}" 'admin@eaas'
+fi
+
