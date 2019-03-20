06b44935f3b0cbc3965489f785da5bb03143ccba
Oleg Stobbe <oleg@openslx.com>
2019-03-20 17:15:15 +0100

    fix problems with preparing installer on macos

--- scripts/ansible-runner.sh
+++ scripts/ansible-runner.sh
@@ -5,7 +5,10 @@ set -e
 repodir="$(cd "$(dirname -- "$0")/.." && pwd -P)"
 workdir='/var/work'
 
-exec docker run --rm --tty --interactive --name eaas-ansible \
+# check if sudo is required to run docker
+docker info > /dev/null 2>&1 || sudocmd='sudo'
+
+exec ${sudocmd} docker run --rm --tty --interactive --name eaas-ansible \
     --volume "${repodir}:${workdir}" \
     --workdir "${workdir}" \
     eaas/ansible \
