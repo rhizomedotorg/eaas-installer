2ac02adb674b7217699d71a93e1bc2d61b1b4337
Oleg Stobbe <oleg@openslx.com>
2019-03-29 17:11:13 +0100

    fix update.sh to be compatible with macos

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,9 +3,8 @@
 __components_as_json() {
     # convert a list of space-separated strings
     # to a JSON object containing an array
-    echo -n '{"eaas_update_components":["'
-    echo -n "$@" | sed 's/ /","/g'
-    echo -n '"]}'
+    local components="$(echo "$@" | sed 's/ /","/g')"
+    printf '{"eaas_update_components":["%s"]}' "${components}"
 }
 
 repodir="$(cd "$(dirname -- "$0")/.." && pwd -P)"
