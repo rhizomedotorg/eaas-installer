12313575664ef68226acb80a804575e5edf47578
Andrii Kost <andriikost@yahoo.com>
2020-03-19 15:16:14 -0500

    updates template config file

--- scripts/ansible-runner.sh
+++ scripts/ansible-runner.sh
@@ -17,6 +17,7 @@ docker info > /dev/null 2>&1 || sudocmd='sudo'
 
 exec ${sudocmd} docker run --rm --tty --interactive --name eaas-ansible \
     --volume "${repodir}:${workdir}" \
+    # --net=host \ # uncomment if using VPN
     --workdir "${workdir}" \
     eaas/ansible \
     "$@"
