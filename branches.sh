49ee296b26b809e2025c1d96516b7b865276d58c
Jurek <jurek.oberhauser@gmx.net>
2023-03-17 14:30:13 +0100

    change urls to eaas instead of openslx

--- branches.sh
+++ branches.sh
@@ -1,11 +1,11 @@
-
+#!/bin/bash
 _is_backend=${1}
 _branch_name=${2}
 
-if ${_is_backend}  ; then
-    _repo_url="https://gitlab.com/openslx/eaas-server.git"
+if test "${_is_backend}"  = "true"  ; then
+    _repo_url="https://gitlab.com/emulation-as-a-service/eaas-server.git"
 else
-    _repo_url="https://gitlab.com/openslx/demo-ui.git"
+    _repo_url="https://gitlab.com/emulation-as-a-service/demo-ui"
 fi
 
 _check_branch=$(git ls-remote --heads ${_repo_url} ${_branch_name})
