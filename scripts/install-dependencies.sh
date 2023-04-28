134c2bb7faf3f4d1ac9c84bb7fd5187738ff7f2b
Rafael Gieschke <rafael@gieschke.de>
2023-04-28 10:09:25 +0000

    fix: work around problems with urllib3 2.0
    urllib3 2.0 (<https://urllib3.readthedocs.io/en/stable/v2-migration-guide.html>) breaks
    <https://github.com/ansible-collections/community.docker/issues/611> via 
    <https://github.com/docker/docker-py/issues/3113>.

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -11,3 +11,4 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y python-is-python3 \
   || update-alternatives --install /usr/bin/python python /usr/bin/python3 100
 
 pip3 install docker-compose ansible
+pip3 install "urllib3<2"
