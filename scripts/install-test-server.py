a7350440c3227f9cc7d640fc8f564842f21e6a70
Rafael Gieschke <rafael@gieschke.de>
2023-07-19 18:06:18 +0200

    HACK: fix: do not check exit status of deploy.sh

--- scripts/install-test-server.py
+++ scripts/install-test-server.py
@@ -157,7 +157,8 @@ if acmesh:
 yaml_save("artifacts/config/hosts.yaml", hosts)
 yaml_save("artifacts/config/eaasi.yaml", config)
 
-cmd("./scripts/deploy.sh")
+# HACK: for check=False, see https://gitlab.com/emulation-as-a-service/eaas-installer/-/issues/6
+cmd("./scripts/deploy.sh", check=False)
 cmd("chmod", "-R", "ugo=u", "/eaas-home")
 
 auth = ""
